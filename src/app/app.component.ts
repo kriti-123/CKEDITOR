412543c31ed2fd0b2961a9f3e89e9b06da4436ee
kriti-123 <ks11115500@gmail.com>
2024-03-21 13:49:36 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'editor4';
+  public Editor = DecoupledEditor;
+  // public Editor = ClassicEditor;
+
+  public onReady(editor: DecoupledEditor): void {
+    const element = editor.ui.getEditableElement()!;
+    const parent = element.parentElement!;
+
+    parent.insertBefore(editor.ui.view.toolbar.element!, element);
+  }
 }
